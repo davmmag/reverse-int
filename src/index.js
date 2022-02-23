ffee29443c635eb1e16f78002e2152a3e3a14f31
davmmag <96019287+davmmag@users.noreply.github.com>
2022-02-23 16:56:37 +0500

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let arr = String(n).split('');
+    let res = [];
+    for(let i = arr.length - 1; i >=0; i--) {
+        if(arr[i] === '-') continue;
+        res.push(arr[i])
+    }
+    return +res.join('');
 }
